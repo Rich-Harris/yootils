4826fd246b89d09e54946c51678b8b5ca0662e73
Rich Harris <richard.a.harris@gmail.com>
2018-08-19 12:30:37 -0400

    update deps, fix build process

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,9 @@ import pkg from './package.json';
 export default {
 	input: 'src/index.ts',
 	output: [
-		{ file: pkg.main, format: 'umd' },
+		{ file: pkg.main, format: 'umd', name: 'yootils' },
 		{ file: pkg.module, format: 'es' }
 	],
-	name: 'yootils',
 	plugins: [
 		typescript({
 			typescript: require('typescript'),
---
+++ yootils.umd.js
@@ -0,0 +1,102 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (factory((global.yootils = {})));
+}(this, (function (exports) { 'use strict';
+
+    function pickRandom(array) {
+        var i = ~~(Math.random() * array.length);
+        return array[i];
+    }
+
+    // http://bost.ocks.org/mike/shuffle/
+    function shuffle(array) {
+        var m = array.length;
+        // While there remain elements to shuffle…
+        while (m > 0) {
+            // Pick a remaining element…
+            var i = Math.floor(Math.random() * m--);
+            // And swap it with the current element.
+            var t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
+
+    function queue(max) {
+        if (max === void 0) { max = 4; }
+        var items = []; // TODO
+        var pending = 0;
+        function dequeue() {
+            if (pending >= max)
+                return;
+            if (items.length === 0)
+                return;
+            pending += 1;
+            var _a = items.shift(), fn = _a.fn, fulfil = _a.fulfil, reject = _a.reject;
+            var promise = fn();
+            try {
+                promise.then(fulfil, reject).then(function () {
+                    pending -= 1;
+                    dequeue();
+                });
+            }
+            catch (err) {
+                reject(err);
+                pending -= 1;
+                dequeue();
+            }
+            dequeue();
+        }
+        return {
+            add: function (fn) {
+                return new Promise(function (fulfil, reject) {
+                    items.push({ fn: fn, fulfil: fulfil, reject: reject });
+                    dequeue();
+                });
+            }
+        };
+    }
+
+    function clamp(num, min, max) {
+        return num < min ? min : num > max ? max : num;
+    }
+
+    function random(a, b) {
+        if (b === undefined)
+            return Math.random() * a;
+        return a + Math.random() * (b - a);
+    }
+
+    function linear(domain, range) {
+        var d0 = domain[0];
+        var r0 = range[0];
+        var m = (range[1] - r0) / (domain[1] - d0);
+        return Object.assign(function (num) {
+            return r0 + (num - d0) * m;
+        }, {
+            inverse: function () { return linear(range, domain); }
+        });
+    }
+
+    // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+    function commas(num) {
+        var parts = String(num).split('.');
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        return parts.join('.');
+    }
+
+    // array
+
+    exports.pickRandom = pickRandom;
+    exports.shuffle = shuffle;
+    exports.queue = queue;
+    exports.clamp = clamp;
+    exports.random = random;
+    exports.linearScale = linear;
+    exports.commas = commas;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
